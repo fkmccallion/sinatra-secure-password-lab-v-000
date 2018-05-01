1952ee3ea1c13968fa608cd0ee17d0d13b8ffb38
Frank McCallion <fkmccallion@gmail.com>
2018-05-01 12:48:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     @user = User.find_by(username: params[:username])
-
+    binding.pry
     if @user
       redirect "/account"
     else
