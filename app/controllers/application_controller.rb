67f05933911fd51bd9f625d9a2052d556d1b8feb
Frank McCallion <fkmccallion@gmail.com>
2018-05-01 12:34:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
 
     @user = User.new(username: params[:username], password: params[:password])
 
-    if @user.username == "" || @user.password_digest == nil
+    if @user.username == "" || @user.password_digest == ""
       redirect "/failure"
     else
       redirect "/login"
